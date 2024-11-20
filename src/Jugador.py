e17404ffb15e2725686afd63614575b7363e2a81
geovannyLV <lv.geovanny@gmail.com>
2024-11-20 15:17:51 -0500

    Guardando antes del pull

--- src/Jugador.py
+++ src/Jugador.py
@@ -10,18 +10,22 @@ class Jugador:
     def tomar5Cartas():
         '''Metodo que toma las 5 cartas de la deck al inicio del juego'''
         pass;
+    
     def robarCartaEnTurno():
         '''Metodo para que hace q el jugador tome una carta,
         se asocia con el metodo robarCarta() de Deck'''
+
         pass;
     def jugarCarta():
         '''Metodo para jugar una carta en el tablero,
         se asocia con el metodo aniadirCarta() de Tablero'''
         pass;
+    
     def declararBatalla():
         '''Metodo para iniciar batalla entre cartas,
         se asocia con atacar() de CartaMonstruo'''
         pass;
+    
     def jugarComoMaquina():
         '''Metodo para la instancia Maquina'''
         pass;
